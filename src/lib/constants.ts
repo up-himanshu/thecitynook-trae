ec29443b92b695286e6d83c13d3291adb6afcad6
Himanshu Upadhyaya <up.himanshu@gmail.com>
2025-07-18 16:53:12 +0530

    change base url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,4 @@
-export const BASE_URL = "http://localhost:3005";
+// export const BASE_URL = "http://localhost:3005";
+export const BASE_URL =
+  "https://cnbc1msz45.execute-api.us-east-1.amazonaws.com";
 export const API_KEY = "abc123";
