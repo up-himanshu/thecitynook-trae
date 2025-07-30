7ab07403bc04b00d3a3beac9418fd50d2fd3bf19
Himanshu Upadhyaya <up.himanshu@gmail.com>
2025-07-30 15:38:20 +0530

    cleanup base url related code

--- src/config/api.ts
+++
@@ -1,3 +0,0 @@
-// export const API_BASE_URL = "http://localhost:3005";
-export const API_BASE_URL =
-  "https://cnbc1msz45.execute-api.us-east-1.amazonaws.com";
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,1 @@
-// export const BASE_URL = "http://localhost:3005";
-export const BASE_URL =
-  "https://cnbc1msz45.execute-api.us-east-1.amazonaws.com";
 export const API_KEY = "abc123";
